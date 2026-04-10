ea6a8c53a81e14430f06efd8a4c2a8d581f96ffe
indexmod <23720911+indexmod@users.noreply.github.com>
2026-04-10 20:50:36 +0300

    123

--- public/moderator.js
+++ public/moderator.js
@@ -6,18 +6,23 @@ async function load() {
   list.innerHTML = "";
 
   json.data.forEach(post => {
+
     const item = document.createElement("div");
     item.className = "adminItem";
 
     item.innerHTML = `
-      <div style="flex:1; color:#b8b8b8;">
-        ${post.text?.slice(0, 100) || ""}
+      <img src="${post.image}" class="adminPreview" />
+
+      <div class="adminText">
+        ${post.text?.slice(0, 120) || ""}
       </div>
 
-      <button>delete</button>
+      <div class="adminActions">
+        <button class="deleteBtn">delete</button>
+      </div>
     `;
 
-    item.querySelector("button").onclick = async () => {
+    item.querySelector(".deleteBtn").onclick = async () => {
       await fetch("/api/delete", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
